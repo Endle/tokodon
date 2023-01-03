157e0923fe5c1467e10b28748f6148ed6ec1669a
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2023-01-02 19:34:14 -0500

    Update craft.ps1

--- craft.ps1
+++ craft.ps1
@@ -1,4 +1,4 @@
 
-Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force
 
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/KDE/craft/master/setup/install_craft.ps1'))
